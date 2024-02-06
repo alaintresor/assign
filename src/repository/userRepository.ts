34d1cde33056279140879b4ba5b340dfdec5ea36
teerenzo <renzahoemmanuel8@gmail.com>
2024-02-06 18:15:53 +0200

    structure

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -3,5 +3,10 @@ export class UserRepository {
     public findUserById = async (id: string): Promise<any> => {
       return User.findByPk(id);
     };
+
+    public createUser = async (userData: any): Promise<any> => {
+      return User.create(userData);
+    };
+    
   }
   
